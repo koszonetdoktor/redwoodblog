e8d1a9ea48c09c1ecb61b13c2a80458de8ab78ea
gergohalmi <ghm@planday.com>
2021-05-01 14:40:06 +0200

    blog posts cell

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,18 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
-  return (
-    <>
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-    </>
-  )
+  return <BlogPostsCell />
 }
 
 export default HomePage
